8379667b6881580028530587838d80a5b5506430
Ivan Uravic <iuravic@gmail.com>
2025-12-09 15:17:26 +0100

    V.2

--- newspack-content-diff-migrator.php
+++ newspack-content-diff-migrator.php
@@ -5,7 +5,7 @@
  * Plugin URI:  https://newspack.com
  * Author:      Automattic
  * Author URI:  https://newspack.com
- * Version:     1.0.3
+ * Version:     2.0.0
  *
  * @package  Newspack_Content_Diff_Migrator
  */
