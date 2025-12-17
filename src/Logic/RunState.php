cf91405a206701ddfcd56d58309ef7a76edd6884
Ivan Uravic <iuravic@gmail.com>
2025-12-17 18:15:59 +0100

    - public const file names - command id filtering and fetching refactored in RunState -- WIP

--- src/Logic/RunState.php
+++ src/Logic/RunState.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * RunState utility for managing execution state in formatted files in the "run-state" directory.
+ * Manages migration run-state data and progress.
  *
  * @package Newspack_Content_Diff_Migrator
  */
@@ -8,21 +8,21 @@
 namespace Newspack\ContentDiffMigrator\Logic;
 
 /**
- * Manages migration run-state data. This data is kept in formatted files in $run_state_dir path.
- * Run-state data is information about all the IDs/objects which will be migrated, have already been migrated,
- * so in case the migration is interrupted, it will be resumed based on this info.
+ * RunState keeps track of IDs/objects which needs to be migrated, and the progress of the migration.
+ * If the migration is interrupted, it will be resumed based on this info.
+ * This data is kept in formatted JSON/JSONL files in $run_state_dir path.
  */
 class RunState {
 
 	// Run-state filenames.
-	private const FILE_NEW_IDS              = 'new_ids.json';
-	private const FILE_MODIFIED_IDS         = 'modified_ids.json';
-	private const FILE_MANIFEST             = 'manifest.json';
-	private const FILE_IMPORTED_POSTS       = 'imported_posts.jsonl';
-	private const FILE_UPDATED_PARENTS      = 'updated_parents.jsonl';
-	private const FILE_UPDATED_FEATURED     = 'updated_featured.jsonl';
-	private const FILE_UPDATED_BLOCKS       = 'updated_blocks.jsonl';
-	private const FILE_DELETED_MODIFIED_IDS = 'deleted_modified_ids.jsonl';
+	public const FILE_NEW_IDS              = 'new_ids.json';
+	public const FILE_MODIFIED_IDS         = 'modified_ids.json';
+	public const FILE_MANIFEST             = 'manifest.json';
+	public const FILE_IMPORTED_POSTS       = 'imported_posts.jsonl';
+	public const FILE_UPDATED_PARENTS      = 'updated_parents.jsonl';
+	public const FILE_UPDATED_FEATURED     = 'updated_featured.jsonl';
+	public const FILE_UPDATED_BLOCKS       = 'updated_blocks.jsonl';
+	public const FILE_DELETED_MODIFIED_IDS = 'deleted_modified_ids.jsonl';
 
 	/**
 	 * Run-state directory path.
@@ -44,15 +44,6 @@ class RunState {
 	}
 
 	/**
-	 * Gets new IDs to be migrated.
-	 *
-	 * @return array|null Array of IDs or null if file doesn't exist.
-	 */
-	public function read_new_ids(): ?array {
-		return $this->read_json( self::FILE_NEW_IDS );
-	}
-
-	/**
 	 * Writes new IDs to be migrated.
 	 *
 	 * @param array $ids Array of IDs.
@@ -64,12 +55,50 @@ class RunState {
 	}
 
 	/**
+	 * Gets new IDs to be migrated.
+	 *
+	 * @return array|null Array of IDs, or null if file doesn't exist.
+	 */
+	public function get_new_ids(): ?array {
+		$new_ids = $this->read_json( self::FILE_NEW_IDS );
+		if ( null === $new_ids ) {
+			return null;
+		}
+		$new_ids = array_map( 'intval', $new_ids );
+		return $new_ids;
+	}
+
+	/**
 	 * Gets modified IDs ('live_id', 'local_id' pairs).
 	 *
-	 * @return array Array of modified ID pairs.
+	 * @return array|null Array of modified ID pairs or null if file doesn't exist.
 	 */
-	public function read_modified_ids(): array {
-		return $this->read_json( self::FILE_MODIFIED_IDS ) ?? [];
+	public function read_modified_ids(): ?array {
+		$modified_ids = $this->read_json( self::FILE_MODIFIED_IDS );
+		if ( null === $modified_ids ) {
+			return null;
+		}
+		array_map( 'intval', $modified_ids );
+		return $modified_ids;
+	}
+
+	/**
+	 * Gets a map of "old => new" modified IDs.
+	 *
+	 * @return array|null Keys are old live modified IDs, values are new local modified IDs, or null if file with modified IDs doesn't exist.
+	 */
+	public function get_modified_ids_map(): ?array {
+		$modified_ids_data = $this->read_modified_ids();
+		if ( null === $modified_ids_data ) {
+			return null;
+		}
+		
+		$ids_map = [];
+		foreach ( $modified_ids_data as $entry ) {
+			$ids_map[ $entry['live_id'] ] = $entry['local_id'];
+		}
+
+		return $ids_map;
 	}
 
 	/**
@@ -85,6 +114,8 @@ class RunState {
 
 	/**
 	 * Writes migration manifest data.
+	 * It's basically just a "migration Table of Contents",
+	 * so that it's easy to review when the migration was done and what was migrated.
 	 *
 	 * @param array $manifest Manifest data.
 	 *
@@ -94,13 +125,70 @@ class RunState {
 		return $this->write_json( self::FILE_MANIFEST, $manifest );
 	}
 
+
+	/**
+	 * Appends a deleted modified ID record.
+	 *
+	 * @param array $deleted_data Deleted data with 'local_id' key.
+	 *
+	 * @return bool Success.
+	 */
+	public function append_deleted_modified_id( array $deleted_data ): bool {
+		return $this->append_jsonl( self::FILE_DELETED_MODIFIED_IDS, $deleted_data );
+	}
+
+	/**
+	 * Gets deleted modified IDs.
+	 *
+	 * @return array Array of deleted modified ID records.
+	 */
+	private function read_deleted_modified_ids(): array {
+		return $this->read_jsonl( self::FILE_DELETED_MODIFIED_IDS );
+	}
+
+	/**
+	 * Gets a map of "old => new" already deleted modified IDs from the run-state file.
+	 *
+	 * @return array|null Keys are old live deleted IDs, values are new local deleted IDs, or null if file with deleted modified IDs doesn't exist.
+	 */
+	public function get_deleted_modified_ids_map(): ?array {
+		$deleted_modified_ids_data = $this->read_deleted_modified_ids();
+		if ( null === $deleted_modified_ids_data ) {
+			return null;
+		}
+
+		$deleted_modified_ids_map = [];
+		foreach ( $deleted_modified_ids_data as $entry ) {
+			$deleted_modified_ids_map[ (int) $entry['live_id'] ] = (int) $entry['local_id'];
+		}
+
+		return $deleted_modified_ids_map;
+	}
+
 	/**
 	 * Gets imported posts.
 	 *
 	 * @return array Array of imported post records.
 	 */
-	public function read_imported_posts(): array {
+	private function read_imported_posts(): array {
 		return $this->read_jsonl( self::FILE_IMPORTED_POSTS );
+	}
+
+	/**
+	 * Gets a map of "old => new" already imported post IDs.
+	 *
+	 * @return array|null Keys are old live imported IDs, values are new local imported IDs, or null if file with imported posts doesn't exist.
+	 */
+	public function get_imported_post_ids_map(): ?array {
+		$imported_posts_data = $this->read_imported_posts();
+		if ( null === $imported_posts_data ) {
+			return null;
+		}
+		$imported_post_ids_map = [];
+		foreach ( $imported_posts_data as $entry ) {
+			$imported_post_ids_map[ (int) $entry['id_old'] ] = (int) $entry['id_new'];
+		}
+		return $imported_post_ids_map;
 	}
 
 	/**
@@ -150,17 +238,6 @@ class RunState {
 	 */
 	public function read_updated_blocks(): array {
 		return $this->read_jsonl( self::FILE_UPDATED_BLOCKS );
-	}
-
-	/**
-	 * Appends a deleted modified ID record.
-	 *
-	 * @param array $deleted_data Deleted data with 'local_id' key.
-	 *
-	 * @return bool Success.
-	 */
-	public function append_deleted_modified_id( array $deleted_data ): bool {
-		return $this->append_jsonl( self::FILE_DELETED_MODIFIED_IDS, $deleted_data );
 	}
 
 	/**
