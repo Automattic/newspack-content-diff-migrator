eb31f9d7c14d11c495acc3c55f63ba7a74c61308
Ivan Uravic <iuravic@gmail.com>
2026-01-23 15:06:04 +0100

    Sanitize query parameter

--- src/Utils/SLAHelper.php
+++ src/Utils/SLAHelper.php
@@ -38,9 +38,10 @@ class SLAHelper {
 	 */
 	public function get_avatar_meta_by_user_id( string $table_prefix ): array {
 		// phpcs:disable -- WordPress.DB.DirectDatabaseQuery.DirectQuery WordPress.DB.DirectDatabaseQuery.NoCaching WordPress.DB.PreparedSQL.InterpolatedNotPrepared.
-		$results = $this->wpdb->get_results(
+		$table_prefix_escaped = esc_sql( $table_prefix );
+		$results              = $this->wpdb->get_results(
 			$this->wpdb->prepare(
-				"SELECT user_id, meta_value FROM {$table_prefix}usermeta WHERE meta_key = %s",
+				"SELECT user_id, meta_value FROM {$table_prefix_escaped}usermeta WHERE meta_key = %s",
 				self::AVATAR_META_KEY 
 			),
 			ARRAY_A
