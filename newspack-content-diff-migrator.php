4490866f5d898cc13ad01b42c45364c645b1c181
Ivan Uravic <iuravic@gmail.com>
2026-04-15 13:19:45 +0200

    Bump up version number

--- newspack-content-diff-migrator.php
+++ newspack-content-diff-migrator.php
@@ -5,7 +5,7 @@
  * Plugin URI:  https://newspack.com
  * Author:      Automattic
  * Author URI:  https://newspack.com
- * Version:     2.1.0
+ * Version:     2.1.1
  *
  * @package  Newspack_Content_Diff_Migrator
  */
